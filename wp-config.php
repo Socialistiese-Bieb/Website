a9f81ce273832cbeaa26212b0e46ac4eb8a01d4a
Socialistiese-Bieb <123833776+Socialistiese-Bieb@users.noreply.github.com>
2023-01-29 00:32:31 +0100

    plugin gefixt. Basisversie website.

--- wp-config.php
+++ wp-config.php
@@ -37,7 +37,6 @@ define( 'DB_CHARSET', 'utf8mb4' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
-define('DISABLE_WP_CRON', false);
 
 /**#@+
  * Authentication unique keys and salts.
